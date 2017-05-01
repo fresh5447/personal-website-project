59b88818198d11baad265e16c2b94f537a0fe484
fresh5447 <doug@kosmojo.com>
2017-05-01 13:50:09 -0600

    CRUD for article

--- routes/articles.js
+++ routes/articles.js
@@ -1,7 +1,6 @@
 const Article = require('../models/article');
 
-exports.all = (req, res) => {
-  console.log('about to find all article')
+exports.getAll = (req, res) => {
   Article.find((err, data) => {
     if (err) return res.send(err, "Error finding all Articles");
     res.json({ message: "Found Articles", data });
@@ -9,7 +8,18 @@ exports.all = (req, res) => {
 };
 
 
-exports.create = (req, res) => {
+exports.getOne = (req, res) => {
+  Article.findById(req.params.article_id, (err, data) => {
+    if (err || !data) {
+      res.json({err: err, message: "No Data Found With That ID"});
+    } else {
+      res.json({message: "Article Found", data});
+    }
+  })
+}
+
+
+exports.createOne = (req, res) => {
   const newArticle = new Article({
     title: req.body.title,
     content: req.body.content
@@ -18,5 +28,32 @@ exports.create = (req, res) => {
   newArticle.save((err, data) => {
     if (err) return res.send(err, "Error creating a new database");
     res.json({message: "Article Created", data})
+  })
+}
+
+
+exports.removeOne = (req, res) => {
+  Article.remove({ _id: req.params.article_id }, (err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({message: "Article Destroyed"});
+    }
+  })
+}
+
+
+exports.editOne = (req, res) => {
+  Article.findById(req.params.article_id , (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      data.title = req.body.title ? req.body.title : data.title;
+      data.content = req.body.content ? req.body.content : data.content;
+      data.save((err, response) => {
+        if(err) res.send(err)
+        res.json(response);
+      })
+    }
   })
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const ArticleRoutes = require('./articles')
 
 /* GET test route. */
 module.exports = (app) => {
-  app.post('/api/articles', ArticleRoutes.create);
-  app.get('/api/articles', ArticleRoutes.all);
+  app.post('/api/articles', ArticleRoutes.createOne);
+  app.get('/api/articles', ArticleRoutes.getAll);
+  app.get('/api/articles/:article_id', ArticleRoutes.getOne);
+  app.delete('/api/articles/:article_id', ArticleRoutes.removeOne);
+  app.put('/api/articles/:article_id', ArticleRoutes.editOne);
 };
